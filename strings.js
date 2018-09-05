8b66747abfe988116b8f196b0cff07a83e224dfc
unknown <sumeet@uxgorilla.com>
2018-09-05 22:56:19 +0530

    more

--- strings.js
+++ strings.js
@@ -15,3 +15,23 @@ if(str1.includes('fine')){
 }else{
     console.log('Not found');
 }
+
+//endsWith
+    console.log(str1.endsWith('fine'));
+    console.log(str1.endsWith('Sharma',6))
+
+//indexOf
+    var paragraph = 'The quick brown fox jumped over the lazy dog. If the dog barked, was it really lazy?';
+
+    console.log(paragraph.indexOf('lazy'));
+
+    console.log(paragraph.indexOf('dog',paragraph.indexOf('dog')+1));//the index of second dog from the first dog
+
+//lastIndexOf
+console.log(paragraph.lastIndexOf('dog'))
+
+//localeCompare
+
+    var str3 = "ab";
+    var str4 = "CD";
+    console.log(str3.localeCompare(str4));
