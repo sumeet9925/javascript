8a5356c2a7c6a6909cdbec23a6941da803866e36
unknown <sumeet@uxgorilla.com>
2018-09-05 22:13:28 +0530

    charCodeAt, concat, includes

--- strings.js
+++ strings.js
@@ -1,4 +1,17 @@
-var str = "sumeet";
+var str = "Sumeet";
+var str1 ="Sharma fine";
+//charAt
+    console.log(str.charAt(0));
+    console.log(str[1]);
+//charCodeAt
+    console.log( str.charCodeAt(0) + " is equals to the value " + str.charAt(0) );
 
-console.log(str.charAt(0));
-console.log(str[1]);
+//concat
+console.log(str.concat(" ", str1))
+
+//includes
+if(str1.includes('fine')){
+    console.log('found');
+}else{
+    console.log('Not found');
+}
